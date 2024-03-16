84bae570642f1e6d969aab6df0ef5ca97856f4ba
Nikson Thapa <niksonthapa11@gmail.com>
2024-03-15 19:10:50 -0500

    Issue #5 has been addressed; a new Java program has been created that can handle user input of their SSN and validate that it is in correct format.

---
+++ JDBC-PSQL-Interaction/ssnInputDemo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ssnInputDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean ask = true;
+        String ssn = "";
+
+        while (ask) {
+            ask = (0 == ssnValidifier(ssn = getUserInput(scan))) ? false : true;
+            if (ask) System.out.println("ERROR: INVALID SSN!");
+        }
+        System.out.println(String.format("Your SSN is: %s", ssn));
+    }
+
+    public static int ssnValidifier(String ssn) {
+        return Pattern.compile("[0-9]{3}-[0-9]{3}-[0-9]{4}")
+                .matcher(ssn)
+                .matches() ? 0 : 1;
+    }
+
+    public static String getUserInput(Scanner scan) {
+        System.out.println("Please enter your SSN (EX: 334-731-3285): ");
+        String ssn = scan.nextLine();
+        return ssn;
+    }
+}
