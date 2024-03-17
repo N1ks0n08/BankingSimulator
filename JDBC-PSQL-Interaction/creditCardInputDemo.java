3b574919854cfebe4cc50f62be07d26e2cd53bb4
Nikson Thapa <niksonthapa11@gmail.com>
2024-03-17 18:01:34 -0500

    Issue #6 has been addressed; addition of a new Java program that checks if the credit card number is 16 digits

---
+++ JDBC-PSQL-Interaction/creditCardInputDemo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class creditCardInputDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean ask = true;
+        String creditCardNumber = "";
+
+        while (ask) {
+            if (0 == creditCardNumberValidifier(creditCardNumber = getCreditCardNumber(scan))) break;
+            System.out.println("ERROR: INVALID CREDIT CARD NUMBER!");
+        }
+
+        System.out.println(String.format("Your credit card number is: %s", creditCardNumber));
+    }
+
+    public static int creditCardNumberValidifier(String creditCardNumber) {
+        return Pattern.compile("[0-9]{16}")
+                .matcher(creditCardNumber)
+                .matches() ? 0 : 1;
+    }
+    
+    public static String getCreditCardNumber(Scanner scan) {
+        System.out.println("Please enter your credit card number: ");
+        System.out.println("EX: 9307298522042466");
+        String creditCardNumber = scan.nextLine();
+        return creditCardNumber;
+    }
+}
